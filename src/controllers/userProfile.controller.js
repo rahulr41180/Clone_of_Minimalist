e5cdd51d8f3798032b1e09997ea6fb555ead43db
Devashish Kumar <devashish.kumar8445@gmail.com>
2022-04-03 18:20:29 +0530

    CSS Changes

---
+++ src/controllers/userProfile.controller.js
@@ -0,0 +1,12 @@
+
+const express = require("express");
+
+const User1 = require("../models/register.model");
+
+const Cart1 = require("../models/cart.model");
+
+const OurProduct1 = require("../models/ourproduct.model");
+
+const router = express.Router();
+
+module.exports = router;
